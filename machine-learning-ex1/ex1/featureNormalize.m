0736929609f4a9e328d93481f7c7ab0eca5334e4
valentin <valentin.roy@mclaren.com>
2020-04-21 09:04:19 +0100

    Completed ex1 (score 100%)

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -5,34 +5,22 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
+n = size(X, 2);
+
 % You need to set these values correctly
 X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = zeros(1, n);
+sigma = zeros(1, n);     
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-
-
-
-
-
-
+for j=1:n 
+  x = X(:,j);
+  mu(j) = mean(x);
+  sigma(j) = std(x);
+  
+  if (sigma(j) ~= 0)
+    X_norm(:, j) = (x - mu(j)) / sigma(j);
+  endif 
+endfor
 
 % ============================================================
 
--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -3,7 +3,11 @@ function plotData(x, y)
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
 
-figure; % open a new figure window
+%figure; % open a new figure window
+
+plot(x, y, 'rx', 'MarkerSize', 10);
+xlabel('Profit in $10,000s');
+ylabel('Population of City in 10,000s');
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the 
