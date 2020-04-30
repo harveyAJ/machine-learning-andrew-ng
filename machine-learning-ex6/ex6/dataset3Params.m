407b4390542f0489531181fcdf59e7b275cf4748
valentin <valentin.roy@mclaren.com>
2020-04-30 11:19:46 +0100

    Add results to ex6 on SVM application to spam classifier

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -8,26 +8,31 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+sigma_arr = [0.01 0.03 0.1 0.3 1 3 10 30];
+C_arr = [0.01 0.03 0.1 0.3 1 3 10 30];
+errors = zeros(3,1);
 
+for i = 1:length(sigma_arr)
+  for j=1:length(C_arr)
+    currentSigma = sigma_arr(i);
+    currentC = C_arr(j);
+    fprintf(['Case sigma = %f C = %f :\n'], currentSigma, currentC);
+   
+    model= svmTrain(X, y, currentC, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+    pred = svmPredict(model, Xval);
+    currentError = mean(double(pred == yval)) * 100;
+    errors = [errors [currentSigma; currentC; currentError]];
+ 
+    fprintf(['Percentage of error for sigma = %f and C = %f: %f percent\n'], ...
+         currentSigma, currentC, currentError);
+     
+  endfor
+endfor
 
-
-
-
-
+[min_error, index] = max(errors(3,:)); %min error is max accuracy
+sigma = errors(1,index);
+C = errors(2,index);
 
 % =========================================================================
 
--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -7,20 +7,7 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the similarity between x1
-%               and x2 computed using a Gaussian kernel with bandwidth
-%               sigma
-%
-%
-
-
-
-
-
-
-% =============================================================
+diff = x1 - x2;
+sim = exp(-0.5*diff' * diff / sigma^2);
     
 end
